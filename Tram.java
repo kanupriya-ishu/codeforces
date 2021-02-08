f16ae90c58439b22a8fb8e3b220b1e5766dd9f1e
kanupriya-ishu <kanupriya2512@gmail.com>
2021-02-08 22:27:45 +0530

    116A - Tram

---
+++ Tram.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Test {
+
+    public static void main(String args[]) 
+    {
+    	Scanner sc = new Scanner(System.in);
+    	int n = sc.nextInt();
+    	
+    	int capacity = 0;
+    	int passengers = 0;
+    	
+    	for(int i=0; i<n; i++) {
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		
+    		passengers = passengers - a + b;
+    		
+    		if(passengers>capacity) {
+    			capacity = passengers;
+    		}
+    	}
+    	
+    	sc.close();
+    	
+    	System.out.println(capacity);
+    }
+    
+}
